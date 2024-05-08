f3df112475d72cba97439916e9cd588449b8539d
George Misael <georgemisaelgantume@gmail.com>
2024-05-08 19:40:30 +0700

    gua

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-2xl text-white">geoorge</div>
+      <div className="text-2xl text-white">aaa</div>
     </main>
   );
 }
